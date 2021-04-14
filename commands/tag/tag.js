6c5a9d295aa3b3b3dd325bb45d115e13b954f4a1
Restyled.io <commits@restyled.io>
2021-04-14 21:48:49 +0000

    Restyled by clang-format

--- commands/tag/tag.js
+++ commands/tag/tag.js
@@ -1,28 +1,27 @@
-const { Command, Flag } = require('discord-akairo');
+const {Command, Flag} = require('discord-akairo');
 
 class tag extends Command {
-    constructor() {
-        super('tag', {
-           aliases: ['tag'],
-           channel: 'guild',
-           *args() {
+  constructor() {
+    super('tag', {
+      aliases : [ 'tag' ],
+      channel : 'guild',
+      *
+          args() {
             const method = yield {
-                type: [
-                  // [module-id, alias1, alias2...]
-                  ['tag-delete', 'delete'],
-                  ['tag-info', 'info'],
-                  ['tag-edit', 'edit'],
-                  ['tag-send', 'send'],
-                  ['tag-raw', 'raw'],
-                ],
-                default: 'tag-send',
-              };
-              return Flag.continue(method);
-           }
-        });
-
-    }
-    
+              type : [
+                // [module-id, alias1, alias2...]
+                [ 'tag-delete', 'delete' ],
+                [ 'tag-info', 'info' ],
+                [ 'tag-edit', 'edit' ],
+                [ 'tag-send', 'send' ],
+                [ 'tag-raw', 'raw' ],
+              ],
+              default : 'tag-send',
+            };
+            return Flag.continue(method);
+          }
+    });
+  }
 }
 
 module.exports = tag
