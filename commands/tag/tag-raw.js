4020ac4310ab6a2d75dabfb0f867cbbf47a68ff5
Restyled.io <commits@restyled.io>
2021-04-14 21:48:50 +0000

    Restyled by prettier

--- commands/tag/tag-raw.js
+++ commands/tag/tag-raw.js
@@ -1,39 +1,39 @@
-const {Command} = require('discord-akairo');
+const { Command } = require("discord-akairo");
 
 // This commands calls tags raw.
 
 class tagRawCommand extends Command {
   constructor() {
-    super('tag-raw', {
+    super("tag-raw", {
       //    aliases: ['tag-raw'],
-      description : {
-        content : 'Calls raw tags',
-        usage : 'tag-raw <name>',
+      description: {
+        content: "Calls raw tags",
+        usage: "tag-raw <name>",
       },
     });
   }
 
   async exec(message) {
-    const args = message.content.slice(process.env.prefix.length).split(' ');
+    const args = message.content.slice(process.env.prefix.length).split(" ");
     if (args[1] === undefined) {
-      return message.channel.send('You have to say what tag you need!')
+      return message.channel.send("You have to say what tag you need!");
     }
-    let tag = await this.client.tagdb.findOne({where : {name : args[1]}})
+    let tag = await this.client.tagdb.findOne({ where: { name: args[1] } });
     if (!tag) {
-      tag = await this.client.tagdb.findOne({where : {aliases : args[1]}})
+      tag = await this.client.tagdb.findOne({ where: { aliases: args[1] } });
       if (!tag) {
         try {
-          message.delete({timeout : 1500})
-        } catch (e) {
-        }
-        let msg = await message.channel.send('That tag doesn\'t exist!')
-        return msg.delete({timeout : 1500})
+          message.delete({ timeout: 1500 });
+        } catch (e) {}
+        let msg = await message.channel.send("That tag doesn't exist!");
+        return msg.delete({ timeout: 1500 });
       }
     }
-    message.delete()
+    message.delete();
     tag.increment("uses");
     return message.channel.send(
-        `__Raw Tag:__ **${tag.name}**\n \n\`\`\`${tag.text}\`\`\``);
+      `__Raw Tag:__ **${tag.name}**\n \n\`\`\`${tag.text}\`\`\``
+    );
   }
 }
 
--- commands/tag/tag.js
+++ commands/tag/tag.js
@@ -1,27 +1,26 @@
-const {Command, Flag} = require('discord-akairo');
+const { Command, Flag } = require("discord-akairo");
 
 class tag extends Command {
   constructor() {
-    super('tag', {
-      aliases : [ 'tag' ],
-      channel : 'guild',
-      *
-          args() {
-            const method = yield {
-              type : [
-                // [module-id, alias1, alias2...]
-                [ 'tag-delete', 'delete' ],
-                [ 'tag-info', 'info' ],
-                [ 'tag-edit', 'edit' ],
-                [ 'tag-send', 'send' ],
-                [ 'tag-raw', 'raw' ],
-              ],
-              default : 'tag-send',
-            };
-            return Flag.continue(method);
-          }
+    super("tag", {
+      aliases: ["tag"],
+      channel: "guild",
+      *args() {
+        const method = yield {
+          type: [
+            // [module-id, alias1, alias2...]
+            ["tag-delete", "delete"],
+            ["tag-info", "info"],
+            ["tag-edit", "edit"],
+            ["tag-send", "send"],
+            ["tag-raw", "raw"],
+          ],
+          default: "tag-send",
+        };
+        return Flag.continue(method);
+      },
     });
   }
 }
 
-module.exports = tag
+module.exports = tag;
